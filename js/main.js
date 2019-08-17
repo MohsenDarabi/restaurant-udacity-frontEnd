f442ac032f9bf8ba12159e6f21d8d18e4cbfafaf
MohsenDarabi <mohsen.darabi@outlook.de>
2019-08-17 23:40:49 +0200

    create restaurant html

--- js/main.js
+++ js/main.js
@@ -163,30 +163,53 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
+  const link = document.createElement('a');
+  // link.innerHTML = 'View Details';
+  link.href = DBHelper.urlForRestaurant(restaurant);
+  link.className = restaurant.cuisine_type.toLowerCase();
+  link.setAttribute('aria-label', 'Details of ' + restaurant.name + ' restaurant, ' + restaurant.neighborhood);
+  link.tabIndex = '0';
+  li.append(link);
+
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  image.alt = 'Image of ' + restaurant.name + ' restaurant';
+  link.append(image);
+
+  const label = document.createElement('div');
+  label.className = 'restaurant-label';
+  link.append(label);
 
   const name = document.createElement('h1');
+  name.className = 'restaurant-name';
   name.innerHTML = restaurant.name;
-  li.append(name);
+  label.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  label.append(neighborhood);
 
   const address = document.createElement('p');
+  address.className = 'restaurant-address';
   address.innerHTML = restaurant.address;
-  li.append(address);
+  label.append(address);
 
-  const more = document.createElement('a');
-  more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  const hr = document.createElement('hr');
+  label.append(hr);
 
-  return li
-}
+  const rating = document.createElement('span');
+  rating.className = 'rating';
+  rating.innerHTML = 'Rating: ' + restaurantRating(restaurant); //'rating';
+  label.append(rating);
+
+  const type = document.createElement('span');
+  type.className = 'cuisine-type';
+  type.innerHTML = restaurant.cuisine_type;
+  label.append(type);
+
+  return li;
+};
 
 /**
  * Add markers for current restaurants to the map.
