4917d7a250e208c5eee61ac3a320e301a5a30da7
MohsenDarabi <mohsen.darabi@outlook.de>
2019-08-17 23:46:23 +0200

    Remove comminted codes

--- js/main.js
+++ js/main.js
@@ -222,18 +222,6 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     function onClick() {
       window.location.href = marker.options.url;
     }
-    self.markers.push(marker);
   });
-
-} 
-/* addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-} */
+}; 
 
